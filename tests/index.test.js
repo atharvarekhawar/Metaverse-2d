10ca45956c01f8970be28d2e5214ffef4ebf868f
Atharva Rekhawar <atharvarekhawar64@gmail.com>
2024-11-06 11:20:13 +0530

    coded tests for user metadata and user avatars

--- tests/index.test.js
+++ tests/index.test.js
@@ -15,7 +15,9 @@ describe("Authentication", () => {
       });
 
       expect(response.statusCode).toBe(200);
+      expect(response.data.userId).toBeDefined();
 
+      //checking for same username to get 400
       const updatedResponse = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
         username,
         password,
@@ -76,4 +78,139 @@ describe("Authentication", () => {
   });
 });
 
-    
+describe("User MetaData endpoint", () => {
+  let token = "";
+  let avatarId = "";
+
+  beforeAll(async () => {
+    const username = "ath" + Math.random();
+    const password = "123456789";
+    await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      username,
+      password,
+      type: "admin",
+    });
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+      username,
+      password,
+    });
+
+    token = response.data.token;
+
+    const avatarResponse = await axios.post(
+      `${BACKEND_URL}/api/v1/admin/avatar`,
+      {
+        imageUrl:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&s",
+        name: "Timmy",
+      },
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    avatarId = avatarResponse.data.avatarId;
+  });
+
+  test("User can't update their metadata with wrong avatarId", async () => {
+    const response = await axios.post(
+      `${BACKEND_URL}/api/v1/user/metadata`,
+      {
+        avatarId: "123123123",
+      },
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    expect(response.statusCode).toBe(400);
+  });
+
+  test("User can update their metadata with right avatarId", async () => {
+    const response = await axios.post(
+      `${BACKEND_URL}/api/v1/user/metadata`,
+      {
+        avatarId: avatarId,
+      },
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("User can't update their metadata if the user forgets to send Authorization token in header", async () => {
+    const response = await axios.post(`${BACKEND_URL}/api/v1/user/metadata`, {
+      avatarId: avatarId,
+    });
+
+    expect(response.statusCode).toBe(403);
+  });
+});
+
+describe("User avatar info", () => {
+  let avatarId = "";
+  let token = "";
+  let userId = "";
+
+  beforeAll(async () => {
+    const username = "ath" + Math.random();
+    const password = "123456789";
+    const signUpResponse = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      username,
+      password,
+      type: "admin",
+    });
+    userId = signUpResponse.data.userId;
+
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+      username,
+      password,
+    });
+
+    token = response.data.token;
+
+    const avatarResponse = await axios.post(
+      `${BACKEND_URL}/api/v1/admin/avatar`,
+      {
+        imageUrl:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&s",
+        name: "Timmy",
+      },
+      {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    avatarId = avatarResponse.data.avatarId;
+  });
+
+  test("Get back avatar information for the user", async () => {
+    const response = await axios.get(
+      `${BACKEND_URL}/api/v1/user/metadata/bulk?ids=[${userId}]`
+    );
+
+    expect(response.data.avatars.length).toBe(1);
+    expect(response.data.avatars[0].userId).toBe(userId);
+  });
+
+  test("Get back avatar information for all the avatars", async () => {
+    const response = await axios.get(`${BACKEND_URL}/api/v1/avatars`);
+
+    expect(response.data.avatars.length).not.toBe(0);
+    const currentAvatar = response.data.avatars.find((x) => x.id === avatarId);
+    expect(currentAvatar).toBeDefined();
+  });
+
+});
+
+
